e9433452e2262898c96e5516425f6e1ec6ef02ca
WoosungChoi <solut2000@gmail.com>
2020-06-25 14:24:27 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+import Layout from "components/Layout";
+import Bio from "components/Bio";
+import SEO from "components/Seo";
+import { getSortedPosts } from "utils/posts";
+
+export default function Home({ posts }) {
+  return (
+    <Layout>
+      <SEO title="All posts" />
+      <Bio />
+      {posts.map(({ frontmatter: { title, description, date }, slug }) => (
+        <article key={slug}>
+          <header>
+            <h3 className="mb-2">
+              <Link href={"/post/[slug]"} as={`/post/${slug}`}>
+                <a className="text-3xl text-orange-600 no-underline">{title}</a>
+              </Link>
+            </h3>
+            <span className="mb-4 text-xs">{date}</span>
+          </header>
+          <section>
+            <p className="mb-8">{description}</p>
+          </section>
+        </article>
+      ))}
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const posts = getSortedPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
