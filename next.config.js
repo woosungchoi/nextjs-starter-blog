595ef562145f6327ae6e15fa8236f0da225f819c
woosungchoi <solut2000@gmail.com>
2020-06-28 05:09:18 +0900

    add webp support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([optimizedImages]);
+module.exports = withPlugins([
+	[optimizedImages, {
+		inlineImageLimit: 8192,
+		imagesFolder: 'images',
+		imagesName: '[name]-[hash].[ext]',
+		handleImages: ['jpeg', 'png', 'webp'],
+		removeOriginalExtension: false,
+		optimizeImages: true,
+		optimizeImagesInDev: true,
+		mozjpeg: {
+		  quality: 80,
+		},
+		optipng: {
+		  optimizationLevel: 3,
+		},
+		pngquant: false,
+		webp: {
+		  preset: 'default',
+		  quality: 82,
+    },		
+}],		
+]);
