120a502e96d989653cfce1fb6c73d29d327f24b1
woosungchoi <solut2000@gmail.com>
2020-06-29 00:13:56 +0900

    add darkmode

--- components/Layout.js
+++ components/Layout.js
@@ -1,27 +1,53 @@
 import Link from "next/link";
+import React, { useState, useEffect } from 'react'
 import { useRouter } from "next/router";
 import Bio from "components/Bio";
 import SEO from "components/Seo";
+import { Sun, Moon } from 'react-feather'
 
-export default function Layout({ children }) {
+export default function Layout({ children, secondaryPage, noHead = false }) {
   const { pathname } = useRouter();
   const isRoot = pathname === "/";
 
-  const header = isRoot ? (
+  const onLoadTheme = typeof localStorage !== 'undefined' && localStorage.getItem('BLOG_THEME')
+  const [theme, setTheme] = useState(onLoadTheme)
+  const [mounted, setMounted] = useState(false)
+  const switchTheme = () => {
+    const setTo = theme === 'dark' ? 'light' : 'dark'
+
+    setTheme(setTo)
+  }
+
+  useEffect(() => {
+    if (onLoadTheme) return
+
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme('dark')
+    }
+  }, [])
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+
+    localStorage.setItem('BLOG_THEME', theme)
+
+    setMounted(true)
+  }, [theme])
+
+  if (!mounted) return <div />
+
+  const header = (
     <h1 className="mb-8">
       <Link href="/">
-        <a className="text-6xl font-black text-black no-underline">
+        <a className="text-6xl no-underline">
           블로그.
         </a>
       </Link>
-    </h1>
-  ) : (
-    <h1 className="mb-8">
-      <Link href="/">
-        <a className="text-6xl font-black text-black no-underline">
-          블로그.
-        </a>
-      </Link>
+	  <div className="float-right">
+		<button className="theme-switch-button" onClick={() => switchTheme()}>
+            {theme === 'dark' ? <Sun /> : <Moon />}
+        </button>
+	  </div>
     </h1>
   );
 
